609af9c67d714b0eff99b6b28800fbcf3288866d
Michael Sliger <michaelj.sliger@gmail.com>
2020-11-10 12:25:46 -0700

    started on day3/4 content; server responses judging in/correct

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const LanguageService = require('./language-service')
 const { requireAuth } = require('../middleware/jwt-auth')
+const jsonParser = express.json()
 
 const languageRouter = express.Router()
 
@@ -50,9 +51,37 @@ languageRouter
   })
 
 languageRouter
-  .post('/guess', async (req, res, next) => {
-    // implement me
-    res.send('implement me!')
+  .post('/guess', jsonParser, async (req, res, next) => {
+    const db = req.app.get('db');
+    const { guess, word } = req.body;
+    // console.log(guess, word, req.user, req.language);
+    // req.user.id comes from userAuth
+    // and req.language.id comes from middleware
+
+    // UPDATE language SET total_score = total_score + 1 WHERE req.language.user_id = req.user.id
+    // UPDATE words SET correct_count = correct_count + 1 WHERE id === word.id
+
+    // ALSO need to implement memory_value functionality
+    if (guess === word.translation) {
+      const updatedObj = await LanguageService.onCorrectGuess(db, word.id, req.user.id)
+
+      res.json({
+        guess: true,
+        word: updatedObj.updatedWord,
+        language: updatedObj.updatedLang,
+      })
+
+    } else {
+      const updatedObj = await LanguageService.onWrongGuess(db, word.id, req.user.id)
+
+
+      res.json({
+        guess: false,
+        word: updatedObj.updatedWord,
+        language: updatedObj.updatedLang
+      })
+    }
+    
   })
 
 module.exports = languageRouter
--- src/language/language-service.js
+++ src/language/language-service.js
@@ -28,6 +28,42 @@ const LanguageService = {
       )
       .where({ language_id })
   },
+
+  getWord(db, word_id) {
+    return db
+      .from('word')
+      .select('*')
+      .where({ id: word_id })
+  },
+
+  async onCorrectGuess(db, word_id, user_id) {
+    return db
+      .raw(`
+            UPDATE word SET correct_count = correct_count + 1 WHERE id = ${word_id};
+            UPDATE language SET total_score = total_score + 1 WHERE user_id = ${user_id};
+            `)
+      .then(async () => {
+        const updatedLang = await this.getUsersLanguage(db, user_id)
+        const updatedWord = await this.getWord(db, word_id);
+        return {
+          updatedLang,
+          updatedWord: updatedWord[0],
+        }
+      })
+  },
+
+  onWrongGuess(db, word_id, user_id) {
+    return db
+      .raw(`UPDATE word SET incorrect_count = incorrect_count + 1 WHERE id = ${word_id};`)
+      .then(async () => {
+        const updatedLang = await this.getUsersLanguage(db, user_id)
+        const updatedWord = await this.getWord(db, word_id);
+        return {
+          updatedLang,
+          updatedWord: updatedWord[0],
+        }
+      })
+  }
 }
 
 module.exports = LanguageService
